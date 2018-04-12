3b6cbe9f34e05489930cf9644eb5a9e18eb3c1c5
Darío Federico Balmaceda <leschatten@gmail.com>
2018-04-12 10:41:37 -0300

    Corregida para probar la función crop Además ahora no imprime en pantalla la imagen - [ ] corregir operador << para las fotos. Estaba teniendo un problema

--- test.cpp
+++ test.cpp
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 
     for(int y=5;y<5+3;y++)
         for(int x=3;x<3+3;x++)
-            photo(x,y) = 1000;
-    photo(6,2) = 600;
+            photo(x,y) = 512;
+    photo(2,6) = 73;
     for(int y=4;y<4+4;y++)
         for(int x=10;x<10+2;x++)
-            photo(x,y) = 720;
+            photo(x,y) = 256;
     
-    raw::event_vec_t events = photo.findEvents();
+    raw::event_vec_t events = photo.findEvents(250,100);
     for (unsigned int i; i < events.size(); i++)    // Esta es la forma de iterar sobre todos los eventos encontrados en una foto. FEA!
     {
         cout << "Centro en: " << events[i].center<double>()
@@ -25,8 +25,7 @@ int main(int argc, char *argv[])
             << "\tCarga: " << events[i].charge()
             << "\tPixeles Saturados: " << events[i].saturatedPixels(1000) << endl;
     }
-    photo.crop(0,0,20,15).toBitMap_grayscale("output.bmp");
+    photo.crop(events[0].center(), 20, 20).toBitMap_grayscale("./local/test_output.bmp");
 
-    cout << photo.crop(0,0,14,10);
     return 0;
 }
